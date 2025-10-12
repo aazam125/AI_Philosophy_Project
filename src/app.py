f2a609582acaafcc8af86c9d2b54d5289f5d8b31
aazam125 <adamazam890@gmail.com>
2025-10-12 16:43:17 -0400

    Adding API function, Advancing Backend, and Build initial project layout with multi-section HTML

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 # Flask entry point for the AI Philosophy Chatbot web application
 # Handles app initialization and base routing
 
-from flask import Flask, request
+from chatbot import get_response, get_quotes
+from flask import Flask, request, render_template, jsonify
 from pathlib import Path
-from .chatbot import get_response
+
 
 templates_path = Path(__file__).resolve().parent.parent.joinpath('templates')
 static_path = Path(__file__).resolve().parent.parent.joinpath('static')
@@ -14,17 +15,34 @@ app = Flask(__name__, template_folder=str(templates_path), static_folder=str(sta
 # Homepage route 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('index.html')
 
-# Route that the user interacts with Philosophers
-@app.route('/ask', methods = ['GET','POST'])
+# UI for the user interacting with the philosophers
+@app.route('/ask')
 def ask():
-    desired_persona = request.args['persona']
-    user_question = request.args['question']
-    response = get_response(user_question, desired_persona)
-    return response
+    return render_template('ask.html')
+
+# API connecting backend to frontend giving user response from philosophers
+@app.route('/api/ask', methods = ['POST'])
+def api_ask():
+    data = request.get_json()
+    desired_persona = data.get('persona')
+    user_question = data.get('question')
+    return jsonify({"persona": desired_persona, "question": user_question, "answer": get_response(user_question, desired_persona)})
 
 
+# UI for getting philosophy quotes 
+@app.route('/quotes')
+def quotes():
+    return render_template('quotes.html')
+
+# API connecting backend to frontend giving user randomized quotes from selected philosophers
+@app.route('/api/quotes', methods = ['POST'])
+def api_quotes():
+    data = request.get_json()
+    desired_persona = data.get('persona')
+    return jsonify({"persona": desired_persona, "answer": get_quotes(desired_persona)})
+    
 
 
 if __name__ == '__main__':
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,3 +1,6 @@
+import random
+
+
 def get_response(prompt: str, persona: str) -> str:
     if (persona == "Socrates" or persona == "socrates"):
         if (prompt == "What is the nature of justice?"):
@@ -22,5 +25,51 @@ def get_response(prompt: str, persona: str) -> str:
             return "Whatever is bothering you in life remember you have the power to revoke this impression, no longer allow yourself to be pulled like a puppet, be free."
     else:
         return "We do not recognize that name, you will make sure to make a note to add it."
+    
+def get_quotes(persona: str) -> str:
+    random_integer = random.randint(1,5)
+    if (persona == "Socrates" or persona == "socrates"):
+        if (random_integer == 1):
+            return "The unexamined life is not worth living."
+        elif (random_integer == 2):
+            return "Strong minds discuss ideas, average minds discuss events, weak minds discuss people."
+        elif (random_integer == 3):
+            return "Know thyself."
+        elif (random_integer == 4):
+            return "Wise men talk because they have something to say; fools, because they have to say something."
+        elif (random_integer == 5):
+            return "We cannot live better than in seeking to become better."
+        else:
+            return "To be is to do."
+
+            return "The question or statment that you phrase to me can be solved simply, the externals of our life are not so important, what is, is our moral virtue, and that includes not being hurt or injured by external things, as I said, 'Anytus and Meletus can kill, but they cannot harm me', what I meant by this was that harm cannot be inflicted without a person allowing it." 
+    elif (persona == "Plato" or persona == "plato"):
+        if (random_integer == 1):
+            return "The greatest wealth is to live content with little."
+        elif (random_integer == 2):
+            return "The beginning is the most important part of the work."
+        elif (random_integer == 3):
+            return "Be kind, for everyone you meet is fighting a harder battle."
+        elif (random_integer == 4):
+            return "Never discourage anyone...who continually makes progress, no matter how slow."
+        elif (random_integer == 5):
+            return "The measure of a man is what he does with power."
+        else:
+            return "Those who tell the stories rule society."
+
+    elif (persona == "Aurelius" or persona == "aurelius"):
+        if (random_integer == 1):
+            return "The happiness of your life depends upon the quality of your thoughts."
+        elif (random_integer == 2):
+            return "Choose not to be harmed, and you won't feel harmed. Don't feel harmed, and you haven't been."
+        elif (random_integer == 3):
+            return "The impediment to action advances action. What stands in the way becomes the way."
+        elif (random_integer == 4):
+            return "If it is not right, do not do it; if it is not true, do not say it."
+        elif (random_integer == 5):
+            return "Nowhere can man find a quieter or more untroubled retreat than in his own soul."
+        else:
+            return "Tranquility comes when you stop caring what others say, think, or do, and only focus on what you do."
+    
         
 
